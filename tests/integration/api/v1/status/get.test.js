52c34490695d93b9fa8b72b311e21db4a0f16a90
Everton Teixeira <evertontp5@gmail.com>
2025-01-11 23:47:50 -0300

    finalizando o endpoint de status

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,21 @@
 test("GET to api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3002/api/v1/status")
   expect(response.status).toBe(200)
+
+  const responseBody = await response.json();
+
+  expect(responseBody.update_at).toBeDefined();
+  const parsedUpadateAt = new Date(responseBody.update_at).toISOString();
+  expect(parsedUpadateAt).toEqual(responseBody.update_at);
+  
+  const postgreVersion = responseBody.dependencies.database.postgres_version;
+  expect(postgreVersion).toBeDefined();
+  expect(postgreVersion).toEqual('9.3.25');
+
+  const maxConnections = parseInt(responseBody.dependencies.database.max_connections);
+  expect(maxConnections).toBeGreaterThan(0);
+  expect(maxConnections).toEqual(100);
+
+  const actual_connections = responseBody.dependencies.database.actual_connections;
+  expect(actual_connections).toEqual(1);
 })
